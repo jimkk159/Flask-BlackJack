69be30ad4b2b1c41c32a0234fa346e3a115dda03
Jim <jimkk159@gmail.com>
2022-10-20 05:05:47 +0800

    socket debug test

--- backend/socket/socket.py
+++ backend/socket/socket.py
@@ -4,3 +4,7 @@ from app import socketio
 @socketio.on('message')
 def handle_message(message):
     print("Received message: " + message)
+
+@socketio.on('my event')
+def handle_message(message):
+    print("Received my event message: " + message)
